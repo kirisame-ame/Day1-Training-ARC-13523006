b4fa2044198e7e62e06529209206a97a13a94b85
William Andrian Dharma T <williamadt123@gmail.com>
2025-07-19 11:29:05 +0700

    fix: Links

--- src/components/LinkContainer.tsx
+++ src/components/LinkContainer.tsx
@@ -10,11 +10,11 @@ export default function LinkContainer() {
     <div className="mt-3 flex flex-row items-center justify-center gap-5">
       <Link href="https://github.com/kirisame-ame" children={<GithubIcon />} />
       <Link
-        href="https://open.spotify.com/user/kirisame_ame"
+        href="https://open.spotify.com/artist/1dJ95RetZNmck5ab3IdWOL?si=s9ynh-UGTVC9NxSR-FRsiw"
         children={<ImageLink src={spotify_logo} alt="Spotify" />}
       />
       <Link
-        href="https://www.linkedin.com/in/kirisame-ame/"
+        href="https://www.linkedin.com/in/william-andrian/"
         children={<ImageLink src={linkedin_logo} alt="LinkedIn" />}
       />
     </div>
